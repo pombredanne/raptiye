a53e8af3c9736a17779ef6e7b3d682e676a2fbd4
Alper KANAT <alperkanat@raptiye.org>
2011-01-14 03:55:40 +0200

    seperated filters and tags + code improvement
     * filters and tags are seperate files now
      * fixed some indentation problems

--- raptiye/blog/templatetags/blog_filters.py
+++ raptiye/blog/templatetags/blog_filters.py
@@ -17,62 +17,15 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 # 
 
-from datetime import date
 import HTMLParser
 
 from django import template
 from django.conf import settings
 from django.contrib.sites.models import Site
 
-from raptiye.blog.functions import is_app_installed, get_latest_entries
-from raptiye.blog.models import Link
-from raptiye.blog.webcal import WebCalendar
+from raptiye.blog.functions import is_app_installed
 
 register = template.Library()
-
-@register.simple_tag
-def calculate_age():
-    return (date.today() - settings.BIRTH_DATE).days/365
-
-@register.simple_tag
-def construct_calendar(year=date.today().year, month=date.today().month, day=date.today().day):
-    wc = WebCalendar(get_latest_entries(), locale=settings.LOCALES["tr"])
-    return wc.formatmonth(year, month, day)
-
-@register.inclusion_tag('pagination.html', takes_context=True)
-def paginator(context, adjacent_pages=2):
-    """
-    To be used in conjunction with the object_list generic view.
-
-    Adds pagination context variables for use in displaying first, adjacent and
-    last page links in addition to those created by the object_list generic
-    view.
-
-    """
-
-    page_numbers = range(max(1, context['page'] - adjacent_pages), min(context['pages'], context['page'] + adjacent_pages) + 1)
-
-    params = context["request"].GET.copy()
-
-    if params.__contains__("page"):
-        del(params["page"])
-
-    return {
-        'page': context['page'],
-        'pages': context['pages'],
-        'page_numbers': page_numbers,
-        'next': context['next'],
-        'previous': context['previous'],
-        'has_next': context['has_next'],
-        'has_previous': context['has_previous'],
-        'show_first': 1 not in page_numbers,
-        'show_last': context['pages'] not in page_numbers,
-        'query_string': params.urlencode(),
-    }
-
-@register.inclusion_tag('links.html')
-def links():
-    return {'links': Link.objects.all()}
 
 @register.filter
 def emotions(entry):
@@ -163,19 +116,3 @@ def code_colorizer(entry):
 @register.filter
 def is_installed(app):
     return is_app_installed(app)
-
-@register.simple_tag
-def project_name():
-    return settings.PROJECT_NAME
-
-@register.simple_tag
-def version():
-    return settings.VERSION
-
-@register.simple_tag
-def project_subtitle():
-    return settings.PROJECT_SUBTITLE
-
-@register.simple_tag
-def rss_url():
-    return settings.RSS_URL
