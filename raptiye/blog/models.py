25b720ac676aa0d0adea68cb7c9d9528fc07cff4
Alper Kanat <alperkanat@raptiye.org>
2011-03-04 05:15:46 +0200

    updated models to support sites

--- raptiye/blog/models.py
+++ raptiye/blog/models.py
@@ -21,6 +21,7 @@ from django.db import models
 from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.contrib.sites.models import Site
+from django.contrib.sites.managers import CurrentSiteManager
 from django.utils.translation import ugettext_lazy as _
 
 from tagging.fields import TagField
@@ -30,13 +31,13 @@ __all__ = ("Entry", "Link")
 class Entry(models.Model):
     """
     Entry Class
-
+    
     This is the class we hold every post for the blog. It
     can be either in Turkish or English due to the choices
     in settings.
-
+    
     """
-
+    
     title = models.CharField(_(u"Title"), help_text=_(u"Required"), max_length=80)
     datetime = models.DateTimeField(_(u"Publish On"), help_text=_(u"Required"))
     content = models.TextField(_(u"Content"), help_text=_(u"Required.. HTML Allowed.."))
@@ -45,18 +46,33 @@ class Entry(models.Model):
     comments_enabled = models.BooleanField(_(u"Comments Enabled"), default=True)
     slug = models.SlugField(_(u"URL"), max_length=100)
     tags = TagField(_(u"Tags"), help_text=_(u"Seperated by commas"))
-
+    sites = models.ManyToManyField(Site, related_name="entries", verbose_name=_(u"Sites"),
+        help_text=_(u"Required"), db_table="sites_per_entry")
+    template_name = models.CharField(_(u"Template Name"), help_text=_(u"Required."),
+        default="homepage.html", null=True, blank=True, max_length=200)
+    objects = CurrentSiteManager()
+    
     def __unicode__(self):
         return self.title
-
+    
+    def get_sites(self):
+        return "<br>".join([
+            "<a href='http://%s' target='_blank'>%s</a>" % (
+                s.domain,
+                s.name
+            ) for s in self.sites.all()])
+    
+    get_sites.short_description = _(u"Published At")
+    get_sites.allow_tags = True
+    
     def get_url(self):
-        return reverse("blog:show_post", args=[
+        return reverse("blog:show_post", args = [
             self.datetime.year,
             self.datetime.month,
             self.datetime.day,
             self.slug
         ])
-
+    
     class Meta:
         get_latest_by = "datetime"
         ordering = ["-datetime", "title", "content"]
@@ -65,15 +81,15 @@ class Entry(models.Model):
 
 class Link(models.Model):
     'The model that stores the links to outer web sites..'
-
+    
     title = models.CharField(_(u"Title"), max_length="50")
     description = models.CharField(_(u"Description"), max_length="200", blank=True)
     url = models.URLField(_(u"URL"))
     window = models.BooleanField(_(u"Open in new window"), default=False)
-
+    
     def __unicode__(self):
         return self.title
-
+    
     class Meta:
         verbose_name = _(u"Link")
         verbose_name_plural = _(u"Links")
--- raptiye/contrib/flatpages/models.py
+++ raptiye/contrib/flatpages/models.py
@@ -1,11 +1,14 @@
-#-*- coding: utf-8 -*-
+# coding: utf-8
 
 from django.db import models
 from django.conf import settings
 from django.contrib.sites.models import Site
+from django.contrib.sites.managers import CurrentSiteManager
 from django.utils.translation import ugettext_lazy as _
 
 from tagging.fields import TagField
+
+__all__ = ("FlatPage",)
 
 class FlatPage(models.Model):
     url = models.CharField(_(u'URL'), max_length=100, db_index=True)
@@ -13,11 +16,25 @@ class FlatPage(models.Model):
     content = models.TextField(_(u'Content'), blank=True)
     # TODO: implement comments for flatpages
     enable_comments = models.BooleanField(_(u'Enable Comments'))
-    template_name = models.CharField(_(u'Template Name'), max_length=70, blank=True, default="default.html")
-    registration_required = models.BooleanField(_(u'Registration required'), help_text=_(u"If this is checked, only logged-in users will be able to view the page."))
-    sites = models.ManyToManyField(Site)
+    registration_required = models.BooleanField(_(u'Registration required'), 
+        help_text=_(u"If this is checked, only logged-in users will be able to view the page."))
+    sites = models.ManyToManyField(Site, related_name="flatpages", verbose_name=_(u"Sites"),
+        help_text=_(u"Required"), db_table="sites_per_flatpage")
+    template_name = models.CharField(_(u"Template Name"), help_text=_(u"Required."),
+        default="default.html", null=True, blank=True, max_length=200)
     show_on_homepage = models.BooleanField(_(u"Show on HomePage"), default=False)
     tags = TagField(_(u"Tags"), help_text=_(u"Seperated by commas"), null=True, blank=True)
+    objects = CurrentSiteManager()
+    
+    def get_sites(self):
+        return "<br>".join([
+            "<a href='http://%s' target='_blank'>%s</a>" % (
+                s.domain,
+                s.name
+            ) for s in self.sites.all()])
+    
+    get_sites.short_description = _(u"Published At")
+    get_sites.allow_tags = True
     
     class Meta:
         ordering = ('url',)
@@ -26,6 +43,3 @@ class FlatPage(models.Model):
     
     def __unicode__(self):
         return u"%s -- %s" % (self.url, self.title)
-    
-    def get_absolute_url(self):
-        return self.url
