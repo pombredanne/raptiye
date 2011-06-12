f34aeaf457ba6194ddd47c72bc8c0fd04f116ce2
Alper Kanat <alperkanat@raptiye.org>
2011-06-13 02:32:36 +0300

    updated context processors and middlewares for django-svn

--- raptiye/settings.py
+++ raptiye/settings.py
@@ -162,17 +162,20 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.doc.XViewMiddleware',
     'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware'
 )
 
 ROOT_URLCONF = 'raptiye.urls'
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.core.context_processors.auth",
+    "django.contrib.auth.context_processors.auth",
     "django.core.context_processors.debug",
     "django.core.context_processors.i18n",
     "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.contrib.messages.context_processors.messages",
     "django.core.context_processors.request",
-    "django.core.context_processors.csrf",
+    "django.core.context_processors.csrf"
 )
 
 TEMPLATE_DIRS = (
