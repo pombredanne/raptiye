fb3d7fa6aa63c1b5edd665375126c960ab6e3520
Alper Kanat <alperkanat@raptiye.org>
2011-12-19 03:19:34 +0200

    reverted to original manage.py

--- raptiye/manage.py
+++ raptiye/manage.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python2
 
+import imp
+
 from django.core.management import execute_manager
 
 try:
-    import local_settings as settings
+    imp.find_module('settings')  # Assumed to be in the same directory.
 except ImportError:
     import sys
-    
-    sys.stderr.write("Error: Can't find the file 'local_settings.py' in the "
-        "directory containing '%s'.\nIf your settings file is named something "
-        "else please specify it using --settings parameter.\n" % __file__)
+    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n" % __file__)
     sys.exit(1)
+
+import settings
 
 if __name__ == "__main__":
     execute_manager(settings)
-
